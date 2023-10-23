a9d12c145a9e4ed9f2abfd46b8a993d7093695d3
Christivie Mauwa <lisedjuma@gmail.com>
2023-10-23 14:18:26 -0500

    projrct_utilities

--- src/main/java/com/christivie/demo/CalculatorServlet.java
+++ src/main/java/com/christivie/demo/CalculatorServlet.java
@@ -1,21 +1,45 @@
 package com.christivie.demo;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import com.christivie.project.utility.Helpers;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "calculatorServlet", value = "/calculator")
 public class CalculatorServlet extends HttpServlet {
+    private static Map<String, String> results = new HashMap<>();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("You made a GET request!!!");
+        request.getRequestDispatcher("WEB-INF/calculator.jsp").forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request , HttpServletResponse response ) throws ServletException, IOException {
         // made through a form
-        System.out.println("You made a Post request ");
+//        System.out.println("You made a Post request ");
+        String num1 = request.getParameter("num1");
+        String num2 = request.getParameter("num2");
+        add(num1,num2);
+        results.put("num1", num1);
+        results.put("num2", num2);
+        request.setAttribute("results", results);
+        request.getRequestDispatcher("calculator.jsp").forward(request, response);
+
     }
+    public void add(String num1, String num2) {
+        if(Helpers.isANumber(num1) && Helpers.isANumber(num2)) {
+            double n1 = Double.parseDouble(num1);
+            double n2 = Double.parseDouble(num2);
+            results.put("sum", n1+ " +" + num2+ "=" + Helpers.round(n1 + n2));
+        } else{
+            results.put("inavalidNumber", "Please input a valid number");
+        }
+    }
+
 }
